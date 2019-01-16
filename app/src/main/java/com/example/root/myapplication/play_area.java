be5c31be4b495d5e14627715a15768c887d71739
root <B16077@students.iitmandi.ac.in>
2019-01-16 17:53:42 +0530

    intro_screens_ready

--- app/src/main/java/com/example/root/myapplication/play_area.java
+++ app/src/main/java/com/example/root/myapplication/play_area.java
@@ -1,5 +1,6 @@
 package com.example.root.myapplication;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class play_area extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_area);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.play_area_top_fragment, new play_area_top_fragment());
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/root/myapplication/sign_up_fragment.java
+++ app/src/main/java/com/example/root/myapplication/sign_up_fragment.java
@@ -8,10 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.rilixtech.CountryCodePicker;                                             //from GitHub
+
 public class sign_up_fragment extends Fragment {
+
+    CountryCodePicker ccp ;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.sign_up_fragment,container,false);
+        View rootView =inflater.inflate(R.layout.sign_up_fragment,container,false);
+        ccp= (CountryCodePicker) rootView.findViewById(R.id.country_code_picker);
+        return rootView ;
     }
 }
