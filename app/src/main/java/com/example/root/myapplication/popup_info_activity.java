dfb57f2d5d8733e9bf658f21d1ce42572a8001aa
root <B16077@students.iitmandi.ac.in>
2019-02-08 15:32:00 +0530

    Current/this Commit Message :
    Features implemented:
    * Joined route_detail,color and station tables for get_route_detail(int route_id) query of route_info_Dao.(Working Good/Stable).
    * Improved popup_info_activity UI
    
    Further Work:
    * get_route_cost(String start_station_name,String end_station_name) query not working : providing null output
    
    References used:
    *Answer by Fredigato at: https://stackoverflow.com/questions/18207470/adding-table-rows-dynamically-in-android
    *Article by SrjSunny at: https://www.geeksforgeeks.org/joining-three-tables-sql/
    
    Previous on this Branch :
    
    Build errors removed
    
    Feature implementation:
    1.joins on Live data (in Progress,Build warnings need to be dealed with).
    2.Popup window on get info button click , Tab 1 middle fragment play area.
    3.View route on Map Activity added ,appears on Click of view route button on Tab 1 middle fragement play Area.
    
    References:
    *answer by fawaad at :https://stackoverflow.com/questions/45059942/return-type-for-android-room-joins
    *answer by Madhur Bhaiya at:https://stackoverflow.com/questions/942571/using-column-alias-in-where-clause-of-mysql-query-produces-an-error
    *answer by Jamie F at :https://stackoverflow.com/questions/8370114/referring-to-a-column-alias-in-a-where-clause
    * Question/Answer by LEGEND MORTAL at:https://stackoverflow.com/questions/48975559/get-data-from-alias-in-room-database-android
    *Android Architecture Components: Room — Relationships blog by Paulina Szklarska at:https://android.jlelse.eu/android-architecture-components-room-relationships-bf473510c14a
    *other(s)

--- app/src/main/java/com/example/root/myapplication/popup_info_activity.java
+++ app/src/main/java/com/example/root/myapplication/popup_info_activity.java
@@ -8,16 +8,23 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class popup_info_activity extends AppCompatActivity implements View.OnClickListener{
 
-    TextView src_station_text_box,dest_station_text_box,error_message_box,cost_message_box;
+    TextView src_station_text_box,dest_station_text_box,error_message_box,cost_message_box,between_station_name,between_station_color;
     ImageView back_btn;
     String src_station_name = null,dest_station_name = null;
 
     private yaanViewModel yaan_view_model;
     get_route_cost_query_result route_cost = null;
+    List<get_route_detail_query_result> between_stations_details=null;
+
+    TableLayout route_detail_table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,7 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
-        int width = displayMetrics.widthPixels;
+        final int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.8));
@@ -41,6 +48,8 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
         error_message_box = findViewById(R.id.error_box);
         cost_message_box = findViewById(R.id.cost_box);
 
+        route_detail_table = findViewById(R.id.route_detail_table);
+
         src_station_name = getIntent().getStringExtra("src_station");
         dest_station_name = getIntent().getStringExtra("dest_station");
 
@@ -52,15 +61,46 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
 
             yaan_view_model.getRoute_cost(new route_cost_filter_input(src_station_name,dest_station_name)).observe(this, new Observer<get_route_cost_query_result>() {
                 @Override
-                public void onChanged(@Nullable get_route_cost_query_result get_route_cost_query_result) {
+                public void onChanged(@Nullable get_route_cost_query_result route_cost_result) {
 
-                    route_cost = get_route_cost_query_result;
+                    route_cost = route_cost_result;
                     if(route_cost != null) {
                         cost_message_box.setText(String.valueOf(route_cost.getCost()));
                     }
                     else
                     {
                         error_message_box.setText("route_cost is null");
+                    }
+
+                }
+            });
+
+            yaan_view_model.getRoute_detail(new route_detail_filter_input(5)).observe(this, new Observer<List<get_route_detail_query_result>>() {
+                @Override
+                public void onChanged(@Nullable List<get_route_detail_query_result> get_route_detail_query_results) {
+
+                    if(get_route_detail_query_results!=null)
+                    {
+                        for (get_route_detail_query_result between_station : get_route_detail_query_results) {
+                            //cost_message_box.setText(between_station.getColor_name());                                //for debugging only
+                            TableRow row = new TableRow(popup_info_activity.this);
+                            TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+                            row.setLayoutParams(layoutParams);
+                            //row.addView(new TextView(popup_info_activity.this).setText(between_station.getStation_name()));
+
+                            between_station_name = new TextView(popup_info_activity.this);
+                            between_station_name.setText(between_station.getStation_name());
+
+                            between_station_color = new TextView(popup_info_activity.this);
+                            between_station_color.setText(between_station.getColor_name());
+                            row.addView(between_station_name);
+                            row.addView(between_station_color);
+                            route_detail_table.addView(row);
+                        }
+                    }
+                    else
+                    {
+                        error_message_box.setText("route_detail_query_results are null");
                     }
 
                 }
--- app/src/main/java/com/example/root/myapplication/route_info_Dao.java
+++ app/src/main/java/com/example/root/myapplication/route_info_Dao.java
@@ -17,10 +17,10 @@ public interface route_info_Dao {
     @Insert
     void insert_route_detail(route_detail_entity route_detail);
 
-    @Query("SELECT * from (SELECT station.name as src_station_name,  station.name as dest_station_name,route_id,cost FROM route INNER JOIN station ON src_station_id=station.id AND dest_station_id=station.id) as innerTable WHERE src_station_name=:start_station AND dest_station_name=:end_station")    //Alias name not working //Query 1
+    @Query("SELECT * from (SELECT station.name as src_station_name,  station.name as dest_station_name,route_id,cost FROM route INNER JOIN station ON src_station_id=station.id AND dest_station_id=station.id) WHERE src_station_name=:start_station AND dest_station_name=:end_station")    //Alias name not working //Query 1
     LiveData<get_route_cost_query_result> get_route_cost(String start_station,String end_station);
 
-    @Query("SELECT station.name as station_name,color_name FROM route_detail INNER JOIN color ON line_color=color.id INNER JOIN station WHERE route_id = :route_number ORDER BY route_detail.`order` ASC")
+    @Query("SELECT station.name as station_name,color_name FROM route_detail INNER JOIN color ON route_detail.line_color = color.id INNER JOIN station ON route_detail.between_station=station.id WHERE route_id = :route_number ORDER BY route_detail.`order` ASC")
     LiveData<List<get_route_detail_query_result>> get_route_detail_asc(int route_number);
 
     //@Query("SELECT station.name ,color_name FROM route_detail INNER JOIN color ON line_color=color.id INNER JOIN station WHERE route_id = :route_number ORDER BY route_detail.`order` DESC")
