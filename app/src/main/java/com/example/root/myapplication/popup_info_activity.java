80a739eb80ccd49134f3e65d83434914c46edbc0
root <B16077@students.iitmandi.ac.in>
2019-02-08 20:57:04 +0530

    Current/this Commit Message: Features Implemented: *if src and dest station entered by user are same. Case Handled. *displaying the correct set of between station corresponding to src and dest station entered by the user ,in correct order.(Under Development)
    Further Work:
    *if start and end station are swapped compared to there entery in route table.
    
    Previous Commit Message :
    
    Feature Implemented:
    *get_route_cost(String start_station_name,String end_station_name) query now working(Good/Stable)
    
    Further Work:
    *Handle cases like:
        1.if src and dest station entered by user are same.
        2.if start and end station are swapped compared to there entery in route table.
        3.displaying the correct set of between station corresponding to src and dest station entered by the user ,in correct order.
    
    References:
    *http://www1.udel.edu/evelyn/SQL-Class3/SQL3_self.html
    
    Previous Commit Message :
    
    Features implemented:
    * Joined route_detail,color and station tables for get_route_detail(int route_id) query of route_info_Dao.(Working Good/Stable).
    * Improved popup_info_activity UI
    
    Further Work:
    * get_route_cost(String start_station_name,String end_station_name) query not working : providing null output
    
    References used:
    *Answer by Fredigato at: https://stackoverflow.com/questions/18207470/adding-table-rows-dynamically-in-android
    *Article by SrjSunny at: https://www.geeksforgeeks.org/joining-three-tables-sql/
    
    Previous on this Branch :
    
    Build errors removed
    
    Feature implementation:
    1.joins on Live data (in Progress,Build warnings need to be dealed with).
    2.Popup window on get info button click , Tab 1 middle fragment play area.
    3.View route on Map Activity added ,appears on Click of view route button on Tab 1 middle fragement play Area.
    
    References:
    *answer by fawaad at :https://stackoverflow.com/questions/45059942/return-type-for-android-room-joins
    *answer by Madhur Bhaiya at:https://stackoverflow.com/questions/942571/using-column-alias-in-where-clause-of-mysql-query-produces-an-error
    *answer by Jamie F at :https://stackoverflow.com/questions/8370114/referring-to-a-column-alias-in-a-where-clause
    * Question/Answer by LEGEND MORTAL at:https://stackoverflow.com/questions/48975559/get-data-from-alias-in-room-database-android
    *Android Architecture Components: Room — Relationships blog by Paulina Szklarska at:https://android.jlelse.eu/android-architecture-components-room-relationships-bf473510c14a
    *other(s)

--- app/src/main/java/com/example/root/myapplication/popup_info_activity.java
+++ app/src/main/java/com/example/root/myapplication/popup_info_activity.java
@@ -26,6 +26,8 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
 
     TableLayout route_detail_table;
 
+    int flag =0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,11 +55,14 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
         src_station_name = getIntent().getStringExtra("src_station");
         dest_station_name = getIntent().getStringExtra("dest_station");
 
-        if ((src_station_name!=null)&&(dest_station_name!=null))
+
+        if ((src_station_name!=null)&&(dest_station_name!=null)&&(!src_station_name.equals(dest_station_name)))
         {
             yaan_view_model = ViewModelProviders.of(this).get(yaanViewModel.class);
             src_station_text_box.setText(src_station_name);
             dest_station_text_box.setText(dest_station_name);
+
+
 
             yaan_view_model.getRoute_cost(new route_cost_filter_input(src_station_name,dest_station_name)).observe(this, new Observer<get_route_cost_query_result>() {
                 @Override
@@ -66,45 +71,100 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
                     route_cost = route_cost_result;
                     if(route_cost != null) {
                         cost_message_box.setText(String.valueOf(route_cost.getCost()));
+                        yaan_view_model.getRoute_detail(new route_detail_filter_input(route_cost.getRoute_id())).observe(popup_info_activity.this, new Observer<List<get_route_detail_query_result>>() {
+                            @Override
+                            public void onChanged(@Nullable List<get_route_detail_query_result> get_route_detail_query_results) {
+
+                                if(get_route_detail_query_results!=null)
+                                {
+                                    for (get_route_detail_query_result between_station : get_route_detail_query_results) {
+                                        //cost_message_box.setText(between_station.getColor_name());                                //for debugging only
+                                        TableRow row = new TableRow(popup_info_activity.this);
+                                        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+                                        row.setLayoutParams(layoutParams);
+                                        //row.addView(new TextView(popup_info_activity.this).setText(between_station.getStation_name()));
+
+                                        between_station_name = new TextView(popup_info_activity.this);
+                                        between_station_name.setText(between_station.getStation_name());
+
+                                        between_station_color = new TextView(popup_info_activity.this);
+                                        between_station_color.setText(between_station.getColor_name());
+                                        row.addView(between_station_name);
+                                        row.addView(between_station_color);
+                                        route_detail_table.addView(row);
+                                    }
+                                }
+                                else
+                                {
+                                    error_message_box.setText("route_detail_query_results are null");
+                                }
+
+                            }
+                        });
+                        flag =1;
                     }
                     else
                     {
                         error_message_box.setText("route_cost is null");
+                        flag = 0;
                     }
 
                 }
             });
 
-            yaan_view_model.getRoute_detail(new route_detail_filter_input(5)).observe(this, new Observer<List<get_route_detail_query_result>>() {
-                @Override
-                public void onChanged(@Nullable List<get_route_detail_query_result> get_route_detail_query_results) {
+            /*
+            if(flag == 0)
+            {
+                yaan_view_model.getRoute_cost(new route_cost_filter_input(dest_station_name,src_station_name)).observe(this, new Observer<get_route_cost_query_result>() {
+                    @Override
+                    public void onChanged(@Nullable get_route_cost_query_result route_cost_result) {
 
-                    if(get_route_detail_query_results!=null)
-                    {
-                        for (get_route_detail_query_result between_station : get_route_detail_query_results) {
-                            //cost_message_box.setText(between_station.getColor_name());                                //for debugging only
-                            TableRow row = new TableRow(popup_info_activity.this);
-                            TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-                            row.setLayoutParams(layoutParams);
-                            //row.addView(new TextView(popup_info_activity.this).setText(between_station.getStation_name()));
+                        route_cost = route_cost_result;
+                        if(route_cost != null) {
+                            cost_message_box.setText(String.valueOf(route_cost.getCost()));
+                            yaan_view_model.getRoute_detail(new route_detail_filter_input(route_cost.getRoute_id())).observe(popup_info_activity.this, new Observer<List<get_route_detail_query_result>>() {
+                                @Override
+                                public void onChanged(@Nullable List<get_route_detail_query_result> get_route_detail_query_results) {
 
-                            between_station_name = new TextView(popup_info_activity.this);
-                            between_station_name.setText(between_station.getStation_name());
+                                    if(get_route_detail_query_results!=null)
+                                    {
+                                        for (get_route_detail_query_result between_station : get_route_detail_query_results) {
+                                            //cost_message_box.setText(between_station.getColor_name());                                //for debugging only
+                                            TableRow row = new TableRow(popup_info_activity.this);
+                                            TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+                                            row.setLayoutParams(layoutParams);
+                                            //row.addView(new TextView(popup_info_activity.this).setText(between_station.getStation_name()));
 
-                            between_station_color = new TextView(popup_info_activity.this);
-                            between_station_color.setText(between_station.getColor_name());
-                            row.addView(between_station_name);
-                            row.addView(between_station_color);
-                            route_detail_table.addView(row);
+                                            between_station_name = new TextView(popup_info_activity.this);
+                                            between_station_name.setText(between_station.getStation_name());
+
+                                            between_station_color = new TextView(popup_info_activity.this);
+                                            between_station_color.setText(between_station.getColor_name());
+                                            row.addView(between_station_name);
+                                            row.addView(between_station_color);
+                                            route_detail_table.addView(row);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        error_message_box.setText("route_detail_query_results are null");
+                                    }
+
+                                }
+                            });
+                            flag =1;
                         }
-                    }
-                    else
-                    {
-                        error_message_box.setText("route_detail_query_results are null");
-                    }
+                        else
+                        {
+                            error_message_box.setText("route_cost is null");
+                            flag = 0;
+                        }
 
-                }
-            });
+                    }
+                });
+            }
+            */
+
         }
         else if ((src_station_name == null)&&(dest_station_name != null))
         {
@@ -114,6 +174,14 @@ public class popup_info_activity extends AppCompatActivity implements View.OnCli
         {
 
             error_message_box.setText("destination Station not selected");
+        }
+        else if((src_station_name!=null)&&(dest_station_name!=null))
+        {
+            if(src_station_name.equals(dest_station_name)) {
+                error_message_box.setText("Source and destination are choosen to be same");
+                src_station_text_box.setText(src_station_name);
+                dest_station_text_box.setText(dest_station_name);
+            }
         }
         else
         {
